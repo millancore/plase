f57a5ea143751c258e5a7587f83080e27921b31d
Juan Millan <juanes14569@hotmail.com>
2018-12-22 13:28:19 -0500

    Update test for RequestBuilder

--- tests/RequestBuilderTest.php
+++ tests/RequestBuilderTest.php
@@ -35,7 +35,7 @@ class RequestBuilderTest extends TestCase
 
         $this->assertInstanceOf(
             PSERequest::class,
-            $builder->request()
+            $builder->getRequest()
         );
     }
 
